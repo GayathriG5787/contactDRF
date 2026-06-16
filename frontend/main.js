69bc5085e3a26efdd8cab8fff6869cc13b81b4ff
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-16 15:22:11 +0530

    POST of contact done

--- frontend/main.js
+++ frontend/main.js
@@ -1,50 +1,65 @@
-const API_URL = 'http://127.0.0.1:8000/contacts/'
-
-
+const API_URL = "http://127.0.0.1:8000/contacts/";
 
 // Load Contacts
 async function loadContacts() {
-    const response = await fetch(API_URL);
-    // response.json() converts json data into JS object
-    const contacts = await response.json();
-    renderContacts(contacts);
+  const response = await fetch(API_URL);
+  // response.json() converts json data into JS object
+  const contacts = await response.json();
+  renderContacts(contacts);
 }
 
 // Display Contacts
-
 function renderContacts(contacts) {
+  const container = document.getElementById("contactList");
 
-    const container =
-        document.getElementById("contactList");
+  const template = document.getElementById("contact-template");
 
-    const template =
-        document.getElementById(
-            "contact-template"
-        );
+  // All the HTML inside this container will be cleared
+  container.innerHTML = "";
 
-    // All the HTML inside this container will be cleared
-    container.innerHTML = "";
+  contacts.forEach((contact) => {
+    // template.content gives the HTML that is stored inside template tag
+    // cloneNode(true) creates copy of the node
+    const clone = template.content.cloneNode(true);
 
-    contacts.forEach(contact => {
-        // template.content gives the HTML that is stored inside template tag
-        // cloneNode(true) creates copy of the node
-        const clone = template.content.cloneNode(true);
+    // querySelector() finds the first element inside clone that has class contact-name
+    clone.querySelector(".contact-name").textContent = contact.name;
 
-        // querySelector() finds the first element inside clone that has class contact-name
-        clone.querySelector(
-            ".contact-name"
-        ).textContent = contact.name;
+    clone.querySelector(".contact-phone").textContent = contact.phone;
 
-        clone.querySelector(
-            ".contact-phone"
-        ).textContent = contact.phone;
+    clone.querySelector(".contact-email").textContent = contact.email;
 
-        clone.querySelector(
-            ".contact-email"
-        ).textContent = contact.email;
-
-        container.appendChild(clone);
-    });
+    container.appendChild(clone);
+  });
 }
 
 loadContacts();
+
+// Add Contacts (POST)
+document.getElementById("contactForm").addEventListener("submit", addContact);
+
+async function addContact(e) {
+  // To prevent default page reloads before JS finishes
+  e.preventDefault();
+
+  const contact = {
+    name: document.getElementById("name").value,
+
+    phone: document.getElementById("phone").value,
+
+    email: document.getElementById("email").value,
+  };
+
+//   fetch sends an HTTP request
+  await fetch(API_URL, {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(contact),
+  });
+
+  loadContacts();
+}
