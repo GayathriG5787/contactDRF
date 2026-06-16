684924a8fb7f5f17f5ed84a2b080d628d0d64960
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-16 11:59:42 +0530

    Validations for phone number

--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.core.validators import RegexValidator
 
 from django.db import models
 
@@ -7,7 +8,18 @@ class Contact(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # For CharField, max_length is mandatory, but for TextField, it is optional
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(
+        max_length=10,
+        unique= True,
+        validators= [
+            RegexValidator(
+                # r = raw string (don't treat backslashes special)
+                # ^ is start of string, \d is digit (0-9), {10} is exactly 10 digits and $ is end of string
+                regex=r'^\d{10}$', 
+                message='Phone number must contain exactly 10 digits'
+            )
+        ]
+        )
     # This field allowed to be empty when user submits a form or when Django validates a model  
     email = models.EmailField(blank=True)
     
