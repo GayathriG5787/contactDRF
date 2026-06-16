7cb75f93afab24d2425f3b99a6c51862e9486020
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-16 12:08:47 +0530

    name validation in serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,12 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = '__all__'
-        
+    
+    # field level validator, format should be validate_<field_name>
+    # value parameter contains the value of the name field
+    def validate_name(self, value):
+        if len(value.strip()) < 3:
+            raise serializers.ValidationError(
+                "Name must contain at least 3 characters."
+            )
+        return value
